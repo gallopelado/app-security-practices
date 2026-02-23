bf9967e468c281ec766b8937407c45f6e286f8bd
Tío Gallo <juanftp100@gmail.com>
2026-02-23 11:34:20 -0300

    agregando authorities a la base de datos

--- db/sql/create_schema.sql
+++ db/sql/create_schema.sql
@@ -1,6 +1,20 @@
 ----------------schema-----------------
+/*
 create table customers(
                           id bigserial primary key,
                           email varchar(50) not null,
                           pwd varchar(500) not null,
                           rol varchar(20) not null);
+*/
+create table customers(
+    id bigserial primary key,
+    email varchar(50) not null,
+    pwd varchar(500) not null
+);
+
+CREATE TABLE roles(
+    role_name VARCHAR(50) PRIMARY KEY,
+    description VARCHAR(100),
+    id_customer bigint,
+    CONSTRAINT fk_customer FOREIGN KEY (id_customer) REFERENCES customers(id)
+);
--- db/sql/data.sql
+++ db/sql/data.sql
@@ -1,4 +1,16 @@
 -----------------data------------------
-insert into customers (email, pwd, rol) VALUES
-                                            ('admin@email.com', '1', 'admin'),
-                                            ('basic@email.com', '2', 'user');
+INSERT INTO
+    customers (email, pwd)
+VALUES
+    ('account@email.com', '1'),
+    ('cards@email.com', '2'),
+    ('loans@email.com', '3'),
+    ('balance@email.com', '4');
+--
+INSERT INTO
+    roles (role_name, description, id_customer)
+VALUES
+    ('VIEW_ACCOUNT', 'Can view account endpoint', 1),
+    ('VIEW_CARDS', 'Can view cards endpoint', 2),
+    ('VIEW_LOANS', 'Can view loans endpoint', 3),
+    ('VIEW_BALANCE', 'Can view balance endpoint', 4);
